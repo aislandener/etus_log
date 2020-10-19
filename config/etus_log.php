111416cbd45962785818845ef8c1aa0a7ad2d304
Aislan Dener <aislandenersouzavicentini@gmail.com>
2020-10-19 15:27:22 -0300

    🚧️ #CONFIG - Criado o arquivo de Config

---
+++ config/etus_log.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'enable' => env("ETUS_LOG_ENABLE", false),
+    'aws' => [
+        "key_id" => env('ETUS_LOG_AWS_ACCESS_KEY'),
+        "secret_key" => env('ETUS_LOG_AWS_SECRET_KEY'),
+        "region" => env('ETUS_LOG_REGION'),
+        "table" => env('ETUS_LOG_DYNAMO_TABLE_NAME')
+    ]
+];
--- src/EtusLogServiceProvider.php
+++ src/EtusLogServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Support\ServiceProvider;
 class EtusLogServiceProvider extends ServiceProvider{
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/../config/etus_log.php' => app()->basePath('config/etus_log.php'),
+        ]);
     }
 
     public function register()
